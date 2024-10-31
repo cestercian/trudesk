d1b9fb2989d81694488523a639f10933ea3fda52
ft4bhi <psabhiram100@gmail.com>
2024-10-31 14:13:59 +0530

    in TopbarContainer .it will read the notification when page opens.if no notifications it will read 0 notifications

--- src/client/app.jsx
+++ src/client/app.jsx
@@ -12,46 +12,46 @@
  *  Copyright (c) 2014-2019. All rights reserved.
  */
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { applyMiddleware, createStore, compose } from 'redux'
-import { Provider } from 'react-redux'
-import createSagaMiddleware from 'redux-saga'
-import { middleware as thunkMiddleware } from 'redux-saga-thunk'
-import IndexReducer from './reducers'
-import IndexSagas from './sagas'
-import { SingletonHooksContainer } from 'react-singleton-hook'
-import TopbarContainer from './containers/Topbar/TopbarContainer'
-import Sidebar from './components/Nav/Sidebar/index.jsx'
-import ModalRoot from './containers/Modals'
-import renderer from './renderer'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { applyMiddleware, createStore, compose } from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+import { middleware as thunkMiddleware } from 'redux-saga-thunk';
+import IndexReducer from './reducers';
+import IndexSagas from './sagas';
+import { SingletonHooksContainer } from 'react-singleton-hook';
+import TopbarContainer from './containers/Topbar/TopbarContainer';
+import Sidebar from './components/Nav/Sidebar/index.jsx';
+import ModalRoot from './containers/Modals';
+import renderer from './renderer';
 
-import SocketGlobal from 'containers/Global/SocketGlobal'
-import SessionLoader from 'lib2/sessionLoader'
-import HotKeysGlobal from 'containers/Global/HotKeysGlobal'
-import BackupRestoreOverlay from 'containers/Global/BackupRestoreOverlay'
-import ChatDock from 'containers/Global/ChatDock'
+import SocketGlobal from 'containers/Global/SocketGlobal';
+import SessionLoader from 'lib2/sessionLoader';
+import HotKeysGlobal from 'containers/Global/HotKeysGlobal';
+import BackupRestoreOverlay from 'containers/Global/BackupRestoreOverlay';
+import ChatDock from 'containers/Global/ChatDock';
 
-const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware();
 
 /*eslint-disable */
 const composeSetup =
   process.env.NODE_ENV !== 'production' && typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : compose
+    : compose;
 /*eslint-enable */
 
 // if (process.env.NODE_ENV !== 'production') {
-localStorage.setItem('debug', 'trudesk:*') // Enable logger
+localStorage.setItem('debug', 'trudesk:*'); // Enable logger
 // }
 
-const store = createStore(IndexReducer, composeSetup(applyMiddleware(thunkMiddleware, sagaMiddleware)))
+const store = createStore(IndexReducer, composeSetup(applyMiddleware(thunkMiddleware, sagaMiddleware)));
 
 // This is need to call an action from angular
 // Goal: remove this once angular is fully removed
-window.react.redux = { store }
+window.react.redux = { store };
 
-sagaMiddleware.run(IndexSagas)
+sagaMiddleware.run(IndexSagas);
 
 // Mount Globals
 if (document.getElementById('globals')) {
@@ -67,26 +67,26 @@ if (document.getElementById('globals')) {
         <BackupRestoreOverlay />
       </>
     </Provider>
-  )
+  );
 
-  ReactDOM.render(GlobalsRoot, document.getElementById('globals'))
+  ReactDOM.render(GlobalsRoot, document.getElementById('globals'));
 }
 
 const sidebarWithProvider = (
   <Provider store={store}>
     <Sidebar />
   </Provider>
-)
+);
 
-ReactDOM.render(sidebarWithProvider, document.getElementById('sidebar'))
+ReactDOM.render(sidebarWithProvider, document.getElementById('sidebar'));
 
 if (document.getElementById('modal-wrapper')) {
   const RootModal = (
     <Provider store={store}>
       <ModalRoot />
     </Provider>
-  )
-  ReactDOM.render(RootModal, document.getElementById('modal-wrapper'))
+  );
+  ReactDOM.render(RootModal, document.getElementById('modal-wrapper'));
 }
 
 if (document.getElementById('topbar')) {
@@ -94,12 +94,12 @@ if (document.getElementById('topbar')) {
     <Provider store={store}>
       <TopbarContainer />
     </Provider>
-  )
+  );
 
-  ReactDOM.render(TopbarRoot, document.getElementById('topbar'))
+  ReactDOM.render(TopbarRoot, document.getElementById('topbar'));
 }
 
-window.react.renderer = renderer
-window.react.dom = ReactDOM
+window.react.renderer = renderer;
+window.react.dom = ReactDOM;
 
-renderer(store)
+renderer(store);
